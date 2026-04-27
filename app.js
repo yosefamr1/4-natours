5ef7af138be5c37d0d35499a94bb858a9264344e
Youssef's Acer <yaamr2002@gmail.com>
2026-04-28 02:13:20 +0300

    Refactoring Our Routes

--- app.js
+++ app.js
@@ -7,18 +7,16 @@ app.use(express.json()); // Middleware to parse JSON bodies
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
 
-//get request handler
-app.get(`/api/v1/tours`, (req, res) => {
+const getTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         data: {
             tours
         }
     })
-})
+}
 
-//get request handler for specific tour
-app.get(`/api/v1/tours/:id`, (req, res) => {
+const getTour = (req, res) => {
     const id = req.params.id;
     const tour = tours.find(el => el.id === id * 1);
 
@@ -32,10 +30,9 @@ app.get(`/api/v1/tours/:id`, (req, res) => {
         status: 'success',
         data: tour
     })
-})
+}
 
-//post request handler
-app.post(`/api/v1/tours`, (req, res) => {
+const createTour = (req, res) => {
     newId = tours[tours.length - 1].id + 1;
     newTour = Object.assign({ id: newId }, req.body);
     tours.push(newTour);
@@ -46,7 +43,23 @@ app.post(`/api/v1/tours`, (req, res) => {
             }
         })
     });
-})
+}
+// //get request handler
+// app.get(`/api/v1/tours`, getTours)
+
+// //get request handler for specific tour
+// app.get(`/api/v1/tours/:id`, getTour)
+
+// //post request handler
+// app.post(`/api/v1/tours`, createTour)
+
+app.route(`/api/v1/tours`)
+    .get(getTours)
+    .post(createTour)
+
+app.route(`/api/v1/tours/:id`)
+    .get(getTour)
+
 
 const port = 3000;
 app.listen(port, () => {
