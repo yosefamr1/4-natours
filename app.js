ec1e9faa3be258fde6b167852c26f1f79b5f2b6f
Youssef's Acer <yaamr2002@gmail.com>
2026-04-28 01:14:30 +0300

    api post update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json()); // Middleware to parse JSON bodies
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
 
-app.get(`/api/v1/tours`,(req,res)=>{
+app.get(`/api/v1/tours`, (req, res) => {
     res.status(200).json({
         status: 'success',
         data: {
@@ -16,21 +16,21 @@ app.get(`/api/v1/tours`,(req,res)=>{
     })
 })
 
-app.post(`/api/v1/tours`,(req,res)=>{
+app.post(`/api/v1/tours`, (req, res) => {
 
     newId = tours[tours.length - 1].id + 1;
-    newTour = Object.assign({id: newId}, req.body);
+    newTour = Object.assign({ id: newId }, req.body);
     tours.push(newTour);
-    console.log(req.body);
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: newTour
-        }
-    })
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success', data: {
+                tour: newTour
+            }
+        })
+    });
 })
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`listening on port ${port}....`);
+    console.log(`listening on port ${port}....`);
 });
