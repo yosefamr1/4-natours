4f0bf026fb1f197c55be8efb83526cab630066a4
Youssef's Acer <yaamr2002@gmail.com>
2026-04-28 01:08:10 +0300

    get & post api done

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
+const fs = require('fs');
 const express = require('express');
 
-const app = express();
 
-app.get(`/`,(req,res)=>{
-    res.status(200).json("hello from server side")
+const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
+
+app.get(`/api/v1/tours`,(req,res)=>{
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours
+        }
+    })
 })
 
-app.post(`/`,(req,res)=>{
-    res.status(200).json("hello from server side")
+app.post(`/api/v1/tours`,(req,res)=>{
+
+    newId = tours[tours.length - 1].id + 1;
+    newTour = Object.assign({id: newId}, req.body);
+    tours.push(newTour);
+    console.log(req.body);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: newTour
+        }
+    })
 })
 
 const port = 3000;
