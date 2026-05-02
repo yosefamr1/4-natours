6c846cded890fb3bcd6aec987e7370939c7f4808
YoussefTrust <yosefamr2002@icloud.com>
2026-05-02 15:28:15 +0300

    config added

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
+console.log(process.env.NODE_ENV);
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); // Use morgan for logging in development mode
+}
 
 const tourRouter = require('./Routes/tourRouts');
 const userRouter = require('./Routes/userRouts');
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}....`);
 });
-
