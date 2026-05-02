18d8ce5aacc3e7de427de8451f5b349ca4d40ee4
YoussefTrust <yosefamr2002@icloud.com>
2026-05-02 15:55:00 +0300

    dev script & prod script added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+// console.log(process.env);
+
 
 const port = 3000;
 app.listen(port, () => {
