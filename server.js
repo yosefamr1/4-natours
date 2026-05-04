c27f00453787755fb41e7395b141610d239bc40c
Youssef's Acer <yaamr2002@gmail.com>
2026-05-05 02:33:19 +0300

    database added

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
+
+// console.log(process.env);
+
+
+db= process.env.DATABASE.replace('<db_password>', process.env.DATABASE_PASSWORD);
+// console.log(db);
+mongoose.connect(db,).then((con) =>{
+    console.log(con.connections);
+    console.log('DB connection successful!');
+} )
+
 
 const app = require('./app');
 // console.log(process.env);
