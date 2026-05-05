0687e550376c8d3dbe3f2f6a3f5f41a1da330747
YoussefTrust <yosefamr2002@icloud.com>
2026-05-05 21:57:56 +0300

    database added

--- server.js
+++ server.js
@@ -5,18 +5,51 @@ dotenv.config({ path: './config.env' });
 
 // console.log(process.env);
 
-
-db= process.env.DATABASE.replace('<db_password>', process.env.DATABASE_PASSWORD);
+db = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
 // console.log(db);
-mongoose.connect(db,).then((con) =>{
-    console.log(con.connections);
-    console.log('DB connection successful!');
-} )
+mongoose.connect(db).then((con) => {
+  // console.log(con.connections);
+  console.log('DB connection successful!');
+});
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error:', err);
+  });
 
 const app = require('./app');
 // console.log(process.env);
-
 
 const port = 3000;
 app.listen(port, () => {
